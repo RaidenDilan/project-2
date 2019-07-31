7a85a971b4ed61043c58a976df400e877fe97b03
RaidenDilan <raiden18@me.com>
2019-07-31 20:00:40 +0100

    testing assets folder.

--- config/oauth.js
+++ config/oauth.js
@@ -9,27 +9,27 @@ module.exports = {
     getLoginURL() {
       return `${this.loginURL}?client_id=${this.clientId}&scope=${this.scope}`;
     }
-  },
-  instagram: {
-    loginURL: 'https://instagram.com/oauth/authorize',
-    accessTokenURL: 'https://instagram.com/oauth/access_token',
-    profileURL: 'https://api.instagram.com/user',
-    clientId: process.env.PR2_INSTAGRAM_CLIENT_ID,
-    clientSecret: process.env.PR2_INSTAGRAM_CLIENT_SECRET,
-    scope: 'user:email',
-    getLoginURL() {
-      return `${this.loginURL}?client_id=${this.clientId}&scope=${this.scope}`;
-    }
-  },
-  facebook: {
-    loginURL: 'https://www.facebook.com/v2.8/dialog/oauth',
-    accessTokenURL: 'https://graph.facebook.com/v2.8/oauth/access_token',
-    profileURL: 'https://api.facebook.com',
-    clientId: process.env.PR2_FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.PR2_FACEBOOK_CLIENT_SECRET,
-    scope: 'user:email',
-    getLoginURL() {
-      return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=https://polar-plains-57938.herokuapp.com/oauth/facebook`;
-    }
   }
+  // instagram: {
+  //   loginURL: 'https://instagram.com/oauth/authorize',
+  //   accessTokenURL: 'https://instagram.com/oauth/access_token',
+  //   profileURL: 'https://api.instagram.com/user',
+  //   clientId: process.env.PR2_INSTAGRAM_CLIENT_ID,
+  //   clientSecret: process.env.PR2_INSTAGRAM_CLIENT_SECRET,
+  //   scope: 'user:email',
+  //   getLoginURL() {
+  //     return `${this.loginURL}?client_id=${this.clientId}&scope=${this.scope}`;
+  //   }
+  // },
+  // facebook: {
+  //   loginURL: 'https://www.facebook.com/v2.8/dialog/oauth',
+  //   accessTokenURL: 'https://graph.facebook.com/v2.8/oauth/access_token',
+  //   profileURL: 'https://api.facebook.com',
+  //   clientId: process.env.PR2_FACEBOOK_CLIENT_ID,
+  //   clientSecret: process.env.PR2_FACEBOOK_CLIENT_SECRET,
+  //   scope: 'user:email',
+  //   getLoginURL() {
+  //     return `${this.loginURL}?client_id=${this.clientId}&redirect_uri=https://polar-plains-57938.herokuapp.com/oauth/facebook`;
+  //   }
+  // }
 };
--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,5 @@
-const mongoose   = require('mongoose');
-const { dbURI }  = require('../config/environment');
+const mongoose  = require('mongoose');
+const { dbURI } = require('../config/environment');
 
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbURI, { useNewUrlParser: true });
@@ -11,48 +11,38 @@ Resort.collection.drop();
 User.collection.drop();
 
 User
-  .create([
-    {
-      firstname: 'Raiden',
-      lastname: 'Dilan',
-      username: 'raidendilan',
-      email: 'raiden18@me.com',
-      password: 'password',
-      passwordConfirmation: 'password',
-      profileImage: 'http://cdn.onlinewebfonts.com/svg/img_568656.png'
-    },{
-      firstname: 'Luca',
-      lastname: 'Ancelotti',
-      username: 'lucaancelotti',
-      email: 'luca@me.com',
-      password: 'password',
-      passwordConfirmation: 'password',
-      profileImage: 'http://cdn.onlinewebfonts.com/svg/img_568656.png'
-    },
-    // {
-    //   firstname: 'Rawand',
-    //   lastname: 'Dilan',
-    //   username: 'rawanddilan',
-    //   email: 'rawand@me.com',
-    //   password: 'password',
-    //   passwordConfirmation: 'password',
-    //   profileImage: 'http://cdn.onlinewebfonts.com/svg/img_568656.png'
-    // }
-  ])
+  .create([{
+    firstname: 'Raiden',
+    lastname: 'Dilan',
+    username: 'raidendilan',
+    email: 'raiden18@me.com',
+    password: 'password',
+    passwordConfirmation: 'password',
+    profileImage: 'http://cdn.onlinewebfonts.com/svg/img_568656.png'
+  },{
+    firstname: 'Luca',
+    lastname: 'Ancelotti',
+    username: 'lucaancelotti',
+    email: 'luca@me.com',
+    password: 'password',
+    passwordConfirmation: 'password',
+    profileImage: 'http://cdn.onlinewebfonts.com/svg/img_568656.png'
+  }])
   .then((users) => {
     console.log(`${users.length} User(s) created`);
+
     return Resort
       .create([{
         name: 'Verbier',
         website: 'www.skiswissalps.com/index.php/en',
         country: 'Switzerland',
         city: 'Valais',
-        lifts: '35',
+        lifts: '67',
         rating: 4,
         lat: '42.5659',
         lng: '1.6019',
         createdBy: users[0],
-        comment: [ 'Awesome place' ],
+        // comment: [ 'Awesome place' ],
         image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Verbier%2C_Switzerland%2C_in_2011.jpg'
       }, {
         name: 'La Plagne',
@@ -63,8 +53,8 @@ User
         rating: 3,
         lat: '45.5224382',
         lng: '6.6572824',
-        createdBy: users[0], // user[1]
-        comment: [ 'Looks Dope!' ],
+        createdBy: users[1],
+        // comment: ['Looks Dope!'],
         image: 'https://img5.onthesnow.com/image/xl/76/76654.jpg'
       }]);
   })
